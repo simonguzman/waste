6e259210c5c17bc46b19329877f0b1818eb77b77
simonguzman <siguzman@unicauca.edu.co>
2024-10-25 14:20:48 -0500

    correcion de la clase product waste

--- src/main/java/com/waste/myfood/domain/value_objects/ProductWaste.java
+++ src/main/java/com/waste/myfood/domain/value_objects/ProductWaste.java
@@ -3,16 +3,22 @@ package com.waste.myfood.domain.value_objects;
 import java.util.UUID;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ProductWaste {
-    private String productWasteId;
-        private Product product;
+    private String id;
+    private String name;
+    private String category;
+    private double stock;
 
-        public ProductWaste(Product product){
-            this.productWasteId = UUID.randomUUID().toString();
-            this.product = product;
-        }
+    public ProductWaste(String name, String category, double stock){
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.category = category;
+        this.stock = stock;
+    }
         
         /**
      * Devuelve los detalles del producto asociado al desperdicio en formato de cadena.
@@ -20,11 +26,9 @@ public class ProductWaste {
      * @return Una cadena que contiene el ID, nombre, categoría y stock del producto.
      */
     public String detailsProduct() {
-        return String.format("ID: %s, Nombre: %s, Categoria: %s, Stock %d",
-                             product.getId(),
-                             product.getName().getName(),
-                             product.getCategory(),
-                             product.getStock().getAmount());
+        return String.format("ID: %s, Nombre: %s, Categoria: %s, Stock: %.2f",
+                             id, name, category, stock);
+                             
     }
 
 }
